e490630f01b67918f9d1b3de2c099f800c09475d
TSABITUL-AZMI\lenovo <tsabitul.azmi@jiarsi.com>
2026-07-28 14:09:46 +0700

    Fix build error opengraph

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -2,6 +2,7 @@
 import { ImageResponse } from "next/og";
 import { DATA } from "@/data/resume";
 
+export const dynamic = "force-static";
 export const runtime = "edge";
 
 export const alt = DATA.name;
